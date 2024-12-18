c68a4b677421a48a98eb8bfbf73a814c0d3672cb
Student <Student@412-02>
2024-12-18 16:01:05 +0300

    Добавьте файлы проекта.

---
+++ HeavyMetal/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace HeavyMetal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.Label iDLabel;
+            System.Windows.Forms.Label фИОLabel;
+            System.Windows.Forms.Label должностьLabel;
+            this.металлургияDataSet = new HeavyMetal.МеталлургияDataSet();
+            this.сотрудникBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.сотрудникTableAdapter = new HeavyMetal.МеталлургияDataSetTableAdapters.СотрудникTableAdapter();
+            this.tableAdapterManager = new HeavyMetal.МеталлургияDataSetTableAdapters.TableAdapterManager();
+            this.сотрудникBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.сотрудникBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.iDTextBox = new System.Windows.Forms.TextBox();
+            this.фИОTextBox = new System.Windows.Forms.TextBox();
+            this.должностьTextBox = new System.Windows.Forms.TextBox();
+            iDLabel = new System.Windows.Forms.Label();
+            фИОLabel = new System.Windows.Forms.Label();
+            должностьLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.металлургияDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.сотрудникBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.сотрудникBindingNavigator)).BeginInit();
+            this.сотрудникBindingNavigator.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // металлургияDataSet
+            // 
+            this.металлургияDataSet.DataSetName = "МеталлургияDataSet";
+            this.металлургияDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // сотрудникBindingSource
+            // 
+            this.сотрудникBindingSource.DataMember = "Сотрудник";
+            this.сотрудникBindingSource.DataSource = this.металлургияDataSet;
+            // 
+            // сотрудникTableAdapter
+            // 
+            this.сотрудникTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.UpdateOrder = HeavyMetal.МеталлургияDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.СотрудникTableAdapter = this.сотрудникTableAdapter;
+            // 
+            // сотрудникBindingNavigator
+            // 
+            this.сотрудникBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.сотрудникBindingNavigator.BindingSource = this.сотрудникBindingSource;
+            this.сотрудникBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.сотрудникBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.сотрудникBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.сотрудникBindingNavigatorSaveItem});
+            this.сотрудникBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.сотрудникBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.сотрудникBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.сотрудникBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.сотрудникBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.сотрудникBindingNavigator.Name = "сотрудникBindingNavigator";
+            this.сотрудникBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.сотрудникBindingNavigator.Size = new System.Drawing.Size(800, 25);
+            this.сотрудникBindingNavigator.TabIndex = 0;
+            this.сотрудникBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Переместить в начало";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Переместить назад";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Положение";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Текущее положение";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(43, 15);
+            this.bindingNavigatorCountItem.Text = "для {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Общее число элементов";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Text = "Переместить вперед";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveLastItem.Text = "Переместить в конец";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Добавить";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorDeleteItem.Text = "Удалить";
+            // 
+            // сотрудникBindingNavigatorSaveItem
+            // 
+            this.сотрудникBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.сотрудникBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("сотрудникBindingNavigatorSaveItem.Image")));
+            this.сотрудникBindingNavigatorSaveItem.Name = "сотрудникBindingNavigatorSaveItem";
+            this.сотрудникBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.сотрудникBindingNavigatorSaveItem.Text = "Сохранить данные";
+            this.сотрудникBindingNavigatorSaveItem.Click += new System.EventHandler(this.сотрудникBindingNavigatorSaveItem_Click);
+            // 
+            // iDLabel
+            // 
+            iDLabel.AutoSize = true;
+            iDLabel.Location = new System.Drawing.Point(293, 160);
+            iDLabel.Name = "iDLabel";
+            iDLabel.Size = new System.Drawing.Size(21, 13);
+            iDLabel.TabIndex = 1;
+            iDLabel.Text = "ID:";
+            // 
+            // iDTextBox
+            // 
+            this.iDTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.сотрудникBindingSource, "ID", true));
+            this.iDTextBox.Location = new System.Drawing.Point(367, 157);
+            this.iDTextBox.Name = "iDTextBox";
+            this.iDTextBox.Size = new System.Drawing.Size(174, 20);
+            this.iDTextBox.TabIndex = 2;
+            // 
+            // фИОLabel
+            // 
+            фИОLabel.AutoSize = true;
+            фИОLabel.Location = new System.Drawing.Point(293, 186);
+            фИОLabel.Name = "фИОLabel";
+            фИОLabel.Size = new System.Drawing.Size(37, 13);
+            фИОLabel.TabIndex = 3;
+            фИОLabel.Text = "ФИО:";
+            // 
+            // фИОTextBox
+            // 
+            this.фИОTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.сотрудникBindingSource, "ФИО", true));
+            this.фИОTextBox.Location = new System.Drawing.Point(367, 183);
+            this.фИОTextBox.Name = "фИОTextBox";
+            this.фИОTextBox.Size = new System.Drawing.Size(174, 20);
+            this.фИОTextBox.TabIndex = 4;
+            // 
+            // должностьLabel
+            // 
+            должностьLabel.AutoSize = true;
+            должностьLabel.Location = new System.Drawing.Point(293, 212);
+            должностьLabel.Name = "должностьLabel";
+            должностьLabel.Size = new System.Drawing.Size(68, 13);
+            должностьLabel.TabIndex = 5;
+            должностьLabel.Text = "Должность:";
+            // 
+            // должностьTextBox
+            // 
+            this.должностьTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.сотрудникBindingSource, "Должность", true));
+            this.должностьTextBox.Location = new System.Drawing.Point(367, 209);
+            this.должностьTextBox.Name = "должностьTextBox";
+            this.должностьTextBox.Size = new System.Drawing.Size(174, 20);
+            this.должностьTextBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(iDLabel);
+            this.Controls.Add(this.iDTextBox);
+            this.Controls.Add(фИОLabel);
+            this.Controls.Add(this.фИОTextBox);
+            this.Controls.Add(должностьLabel);
+            this.Controls.Add(this.должностьTextBox);
+            this.Controls.Add(this.сотрудникBindingNavigator);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.металлургияDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.сотрудникBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.сотрудникBindingNavigator)).EndInit();
+            this.сотрудникBindingNavigator.ResumeLayout(false);
+            this.сотрудникBindingNavigator.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private МеталлургияDataSet металлургияDataSet;
+        private System.Windows.Forms.BindingSource сотрудникBindingSource;
+        private МеталлургияDataSetTableAdapters.СотрудникTableAdapter сотрудникTableAdapter;
+        private МеталлургияDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator сотрудникBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton сотрудникBindingNavigatorSaveItem;
+        private System.Windows.Forms.TextBox iDTextBox;
+        private System.Windows.Forms.TextBox фИОTextBox;
+        private System.Windows.Forms.TextBox должностьTextBox;
+    }
+}
+
